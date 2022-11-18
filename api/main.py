a69c431e4b839e0a3ffee2fbdfdf32a5a3c4c6b1
maxime.delcourt <maxime.delcort@epita.fr>
2022-11-18 14:58:11 +0100

    read csv to json

--- api/main.py
+++ api/main.py
@@ -18,12 +18,11 @@ import pandas as pd
 import csv
 
 
-response_API = requests.get('https://data.ademe.fr/data-fair/api/v1/datasets/elecdom-donnees-de-consommation-annuelle/lines?size=10000&page=1&q_mode=complete&finalizedAt=2022-03-04T08:11:52.428Z&format=csv')
+response_API = requests.get('https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv')
 
 
-def csv_to_json(csvFilePath, jsonFilePath):
+def csv_to_json(csvFilePath)->json:
     jsonArray = []
-
     # read csv file
     with open(csvFilePath) as csvf:
         # load csv file data using csv library's dictionary reader
@@ -35,9 +34,9 @@ def csv_to_json(csvFilePath, jsonFilePath):
             jsonArray.append(row)
 
     # convert python jsonArray to JSON String and write to file
-    with open(jsonFilePath, 'w') as jsonf:
-        jsonString = json.dumps(jsonArray, indent=4)
-        jsonf.write(jsonString)
+    jsonString = json.dumps(jsonArray)
+    return json.loads(jsonString)
+
 
 
 print(response_API.status_code)
@@ -46,9 +45,8 @@ data = response_API.text
 f = open('temp.csv','w')
 f.write(data)
 f.close()
-f = csv_to_json('temp.csv','jsontest')
-print(data)
-json_data = json.dumps(data)
-parse_json = json.loads(json_data)
+data = csv_to_json('temp.csv')
 
-a=1
+print(data)
+
+aaa=1
