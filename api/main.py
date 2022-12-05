3bf7e91c065017b02199241e00e13256215bd414
Cécile PHILIPPO <cecile.philippo@epita.fr>
2022-12-05 15:49:17 +0100

    # Conflicts: #	.idea/pythonConspy.iml #	.idea/workspace.xml #	api/main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from sqlalchemy.dialects.postgresql import psycopg2
+import csv
+from fastapi import UploadFile
+
 
 import uvicorn
 from fastapi import FastAPI
@@ -27,21 +29,31 @@ import json
 import pandas as pd
 import csv
 import os
-import pyodbc
-
-server = 'conspy.cyqm4cgvogvg.eu-west-3.rds.amazonaws.com'
-database = 'ConsPy'
-username = 'ConsPy'
-password = 'JWC616BuObW1boP0gmQL'
-port = 5432
-
-# aller sur https://docs.aws.amazon.com/fr_fr/AmazonRDS/latest/UserGuide/UsingWithRDS.IAMDBAuth.Connecting.Python.html
-
 
 response_API = requests.get('https://raw.githubusercontent.com/owid/energy-data/master/owid-energy-data.csv')
 
 
-def csv_to_json(csvFilePath) -> json:
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+
+    # read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        # load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf)
+
+        # convert each csv row into python dict
+        for row in csvReader:
+            # add this python dict to json array
+            jsonArray.append(row)
+
+    # convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+
+
+"""
+def csv_to_json(csvFilePath):
     jsonArray = []
     # read csv file
     with open(csvFilePath) as csvf:
@@ -56,23 +68,42 @@ def csv_to_json(csvFilePath) -> json:
     # convert python jsonArray to JSON String and write to file
     jsonString = json.dumps(jsonArray)
     return json.loads(jsonString)
-
+"""
 
 print(response_API.status_code)
 data = response_API.text
 
-f = open('temp.csv', 'w')
-f.write(data)
-f.close()
-data = csv_to_json('temp.csv')
+f = open('api/temp.csv', 'a')
 
-print(data)
-os.remove('temp.csv')
+# f.write(data)  #this create the database
+f.close()
+
+data = csv_to_json('api/temp.csv', 'api/temp.json')
+
+# print(data['wind_cons_change_pct'])
+
+
+a = 1
+
+
+@app.post("/add/csv")
+async def addToDbCsv(toAddCsv: UploadFile):
+    line = toAddCsv.file.readline()
+    f = open('api/temp.csv', 'a')
+    while (line != ''):
+        f.write(line)
+        line = toAddCsv.file.readline()
+    f.close()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def getDataJson():
+    csv_to_json('api/temp.csv', 'api/data.json')
+    f = open('api/data.json')
+    lines = f.readlines()
+    f.close()
+    return lines
+
 
 
 @app.get("/hello/{name}")
